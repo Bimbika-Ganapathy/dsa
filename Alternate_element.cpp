bf27c682beb512536313edc0d1def6af9e3105a4
Bimbika-Ganapathy <bimbikag@gmail.com>
2024-12-19 11:15:32 +0530

    (dsa day 2) Alternate-element

---
+++ Alternate_element.cpp
@@ -0,0 +1,10 @@
+
+class Solution {
+  public:
+    vector<int> getAlternates(vector<int> &arr) {
+        verctor<int>
+        for(int i=0;i<arr.size();i+2){
+           return i;
+        }// code here
+    }
+};
