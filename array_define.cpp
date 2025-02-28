aa4ceddab990bbddf27a696c1802766526768694
Bimbika-Ganapathy <bimbikag@gmail.com>
2025-02-28 22:42:31 +0530

    array

---
+++ array_define.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // int a, b, c , d;
+    // cin >> a >> b >> c >> d;
+    int arr[5];
+
+    cin >> arr[0] >>arr[1] >>arr[2] >> arr[3]>> arr[4]>> arr[5];
+    arr[0]+= 5;
+    cout << arr[0];
+    
+    return 0;
+}
